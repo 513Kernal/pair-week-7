3a90221cc791431dfded5b9700827309239c4cfc
Rachel Clifton <racheljclifton@gmail.com>
2021-02-26 14:20:07 -0500

    week 7 pair exercise submission

--- 15_Review/java/src/main/java/com/techelevator/controller/CatController.java
+++ 15_Review/java/src/main/java/com/techelevator/controller/CatController.java
@@ -1,9 +1,6 @@
 package com.techelevator.controller;
 
-import com.techelevator.model.CatCard;
-import com.techelevator.model.CatCardDAO;
-import com.techelevator.model.CatFact;
-import com.techelevator.model.CatPic;
+import com.techelevator.model.*;
 import com.techelevator.services.CatFactService;
 import com.techelevator.services.CatPicService;
 import org.springframework.http.HttpStatus;
@@ -12,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@RestController
+@RequestMapping("/api/cards")
 public class CatController {
 
     private CatCardDAO catCardDao;
@@ -22,6 +21,51 @@ public class CatController {
         this.catCardDao = catCardDao;
         this.catFactService = catFactService;
         this.catPicService = catPicService;
+    }
+
+    @RequestMapping(path ="", method = RequestMethod.GET)
+    public List<CatCard> getAllCards(){
+        return catCardDao.list();
+    }
+
+    @RequestMapping(path ="/{id}", method = RequestMethod.GET)
+    public CatCard getCard(@PathVariable long id) throws CatCardNotFoundException {
+        return catCardDao.get(id);
+    }
+
+    @RequestMapping(path = "/random", method = RequestMethod.GET)
+    public CatCard generateNewRandomCard() {
+        CatCard card = new CatCard();
+
+        CatFact fact = catFactService.getFact();
+        CatPic pic = catPicService.getPic();
+
+        card.setCatFact(fact.getText());
+        card.setImgUrl(pic.getFile());
+
+        return card;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "", method = RequestMethod.POST)
+    public void save(@Valid @RequestBody CatCard card){
+        catCardDao.save(card);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RequestMapping(path = "/{id}", method = RequestMethod.PUT)
+    public void udpateCard(@Valid @RequestBody CatCard card, @PathVariable long id) throws CatCardNotFoundException {
+        if (!catCardDao.update(id, card)) {
+            throw new CatCardNotFoundException();
+        }
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+    public void deleteCard(@PathVariable long id) throws CatCardNotFoundException {
+        if (!catCardDao.delete(id)) {
+            throw new CatCardNotFoundException();
+        }
     }
 
 
